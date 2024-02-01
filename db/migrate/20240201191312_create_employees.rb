edfc37e4ae50ab613186579e13d659053083bbec
dawson <dawsontimmons@gmail.com>
2024-02-01 13:21:49 -0700

    modify fields that require null and unique

--- db/migrate/20240201191312_create_employees.rb
+++ db/migrate/20240201191312_create_employees.rb
@@ -1,15 +1,18 @@
 class CreateEmployees < ActiveRecord::Migration[7.1]
   def change
     create_table :employees do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :phone
-      t.string :email
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :phone, null: false
+      t.string :email, null: false
       t.string :location
       t.text :skills, array: true, default: []
       t.text :bio
 
       t.timestamps
     end
+
+    add_index :employees, :email, unique: true
+    add_index :employees, :phone, unique: true
   end
 end
--- db/migrate/20240201193146_create_references.rb
+++ db/migrate/20240201193146_create_references.rb
@@ -2,11 +2,11 @@ class CreateReferences < ActiveRecord::Migration[7.1]
   def change
     create_table :references do |t|
       t.references :employee, null: false, foreign_key: true
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :phone
       t.string :email
-      t.string :relationship
+      t.string :relationship, null: false
 
       t.timestamps
     end
