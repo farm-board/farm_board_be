1270cfd05ee07c0ea9fac83b92906460657953b6
dawson <dawsontimmons@gmail.com>
2024-02-08 14:30:55 -0700

    modify postings table to add payment type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_184734) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_201706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_184734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age_requirement"
+    t.string "payment_type"
     t.index ["farm_id"], name: "index_postings_on_farm_id"
   end
 
