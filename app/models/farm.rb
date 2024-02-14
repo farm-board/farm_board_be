df6b0f391c92461e81a0fc40ac18404015ccb413
Dylan Timmons <dylanstimmons@icloud.com>
2024-02-14 15:23:56 -0700

    adds model validation to farms

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,12 @@ class Farm < ApplicationRecord
   has_one :accommodation
   has_many :postings
   has_many :posting_employees, through: :postings
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :image,
+  validates :bio
+  
 end
