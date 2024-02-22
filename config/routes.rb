2bc0fcd2d3c10a61b9704767d41e49e321a1f027
dawson <dawsontimmons@gmail.com>
2024-02-22 14:11:27 -0700

    added new routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   get 'current_user', to: 'current_user#index'
+  patch 'current_user/update', to: 'current_user#update'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   namespace :api do
     namespace :v1 do
-      resources :farms do
-        resources :postings
-        resource :accommodation
-      end
-      resources :employees do
-        resources :experiences
-        resources :references
+      resources :users do
+        resource :farms do
+          resources :postings
+          resource :accommodation
+        end
+        resource :employees do
+          resources :experiences
+          resources :references
+        end
       end
     end
   end
