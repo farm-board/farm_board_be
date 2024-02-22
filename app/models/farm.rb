59a13cf5a7edac183fc6da3727335d6638c2caad
dawson <dawsontimmons@gmail.com>
2024-02-22 14:08:59 -0700

    add creation of farms through current user and associations

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,11 +1,6 @@
 class Farm < ApplicationRecord
+  belongs_to :user
   has_one :accommodation
   has_many :postings
   has_many :posting_employees, through: :postings
-
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
-
 end
