8e6653dee438bb193dd56512995d39d53338d0d7
Dawson Timmons <DawsonTimmons@gmail.com>
2024-06-04 13:33:56 -0600

    add backend changes to accommodate infinite scrolling

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FeedsController < ApplicationController
   def show
-    @postings = Posting.joins(:farm).select('postings.*, farms.name as farm_name, farms.city as farm_city, farms.state as farm_state').all
+    @postings = Posting.joins(:farm).select('postings.*, farms.name as farm_name, farms.city as farm_city, farms.state as farm_state').page(params[:page]).per(4)
     render json: FeedsSerializer.new(@postings)
   end
 end
