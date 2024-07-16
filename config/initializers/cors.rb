98a8ffe0dc594b7323634a06854ca0e3ae3f5263
Dawson Timmons <DawsonTimmons@gmail.com>
2024-07-16 16:18:23 -0600

    changed origin urls to test issue on testflight

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173, https://walrus-app-bfv5e.ondigitalocean.app'
+    origins '*'
 
     resource "*",
       headers: :any,
