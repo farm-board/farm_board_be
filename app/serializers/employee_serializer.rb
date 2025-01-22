ed697af34974a938a74e3fce3df54cb24526de90
Dawson Timmons <DawsonTimmons@gmail.com>
2025-01-22 15:37:36 -0700

    add marketplace info to profile serializers

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,5 +1,5 @@
 class EmployeeSerializer
   include JSONAPI::Serializer
   set_id :id
-  attributes :first_name, :last_name, :city, :state, :zip_code, :skills, :bio, :age, :phone, :email, :main_image, :setup_complete
+  attributes :first_name, :last_name, :city, :state, :zip_code, :skills, :bio, :age, :phone, :email, :main_image, :marketplace_phone, :marketplace_email, :setup_complete
 end
